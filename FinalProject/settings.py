41956de7fd947a0ac7d82fac8c64caecdb7ae6a3
Ivan Fedorov <ivan45sc@gmail.com>
2025-01-18 12:33:05 +0500

    tempaltes added

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -43,7 +44,7 @@ ROOT_URLCONF = 'FinalProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,8 +107,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES  = [
-    BASE_DIR / "main/static"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
