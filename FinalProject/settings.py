45065aba38063e519a525a51a62e66e200bcf0a0
Ivan Fedorov <ivan45sc@gmail.com>
2025-01-19 02:02:46 +0500

    allow hosts

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-@#7o8+mc*#41d)%pntz_-detn_d!0f!h@ttkm2v#*=dblqm2*!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
