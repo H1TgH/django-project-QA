2fc2c366157c7e3c8ba7df9c71dbc265910f249f
Ivan Fedorov <ivan45sc@gmail.com>
2025-01-19 22:17:15 +0500

    added mysql connection

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -71,8 +71,12 @@ WSGI_APPLICATION = 'FinalProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'qaengineer',
+        'USER': 'root',
+        'PASSWORD': 'zewerzewer',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
